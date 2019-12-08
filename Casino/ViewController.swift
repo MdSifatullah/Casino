4dce9aa4dffd1608aca66e97d983260f5a141198
Md Sifat <cse.sifat@gmail.com>
2019-12-08 14:32:17 +0600

    design

--- Casino/ViewController.swift
+++ Casino/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var FirstImageUI: UIImageView!
+    
+    @IBOutlet weak var SecondImageUI: UIImageView!
+    
+    @IBOutlet weak var ThirdImageUI: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func PressBtn(_ sender: Any) {
+       
+                FirstImageUI.image = UIImage(named: "ace_of_spades")
+                SecondImageUI.image = UIImage(named: "ace_of_hearts")
+                ThirdImageUI.image = UIImage(named: "ace_of_clubs")
+    }
 }
 
